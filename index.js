e51b01680457ce97f4eead62e4f89f52058ea7da
AnikaLabiba <anikalabibajahin@gmail.com>
2022-04-30 22:39:39 +0600

    post api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,11 +8,6 @@ const port = process.env.PORT || 5000
 //middlewares
 app.use(cors())
 app.use(express.json())
-
-
-//carParadiseUser
-//cKUVSaXjf4hh8WIs
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z240d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -24,11 +19,27 @@ async function run() {
         const carsCollection = client.db("car-paradise").collection("car");
 
         //getting all the cars data from db
-        app.get('/cars', async (req, res) => {
+        app.get('/inventory', async (req, res) => {
             const query = {}
             const cursor = carsCollection.find(query)
             const cars = await cursor.toArray()
             res.send(cars)
+        })
+
+        //get single car from db
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const carDetails = await carsCollection.findOne(query)
+            res.send(carDetails)
+        })
+
+        //adding new car to the db
+        app.post('/inventory', async (req, res) => {
+            const newCar = req.body
+            const result = await carsCollection.insertOne(newCar)
+            res.send(result)
+            console.log(result)
         })
     }
     finally {
