2f3ebf4682fea5e1e0e957b9a47c64e071816b21
AnikaLabiba <anikalabibajahin@gmail.com>
2022-04-30 12:04:08 +0600

    server is connected to database.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,6 +8,22 @@ const port = process.env.PORT || 5000
 //middlewares
 app.use(cors())
 app.use(express.json())
+
+
+//carParadiseUser
+//cKUVSaXjf4hh8WIs
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z240d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const carsCollection = client.db("car-paradise").collection("car");
+    console.log('Car Paradise server is connected to db.')
+    // perform actions on the collection object
+    client.close();
+});
+
 
 //root api
 app.get('/', async (req, res) => {
