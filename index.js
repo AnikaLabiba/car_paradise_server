32064d1378b616a0cbc7a1d396a8755443e250eb
AnikaLabiba <anikalabibajahin@gmail.com>
2022-05-07 12:03:50 +0600

    jwt api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -9,15 +10,41 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+function varifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded
+        next()
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z240d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+''
 async function run() {
     try {
         await client.connect()
         const carsCollection = client.db("car-paradise").collection("car");
         const ordersCollection = client.db("car-paradise").collection("order");
+
+        //auth and creating jwt
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken })
+        })
 
         //getting all the cars data from db
         app.get('/inventory', async (req, res) => {
@@ -76,12 +103,15 @@ async function run() {
         })
 
         //getting orders by fillering email 
-        app.get('/order', async (req, res) => {
+        app.get('/order', varifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
             const email = req.query.email
-            const query = { email: email }
-            const cursor = ordersCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = ordersCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
         })
 
         //cancel order
